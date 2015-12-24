6d36fa7e972984e6f49c192e7ac07ce6b8b906cf
gexiangying <19727709@qq.com>
2015-12-24 08:35:02 +0800

    first commit

---
+++ config.lua
@@ -0,0 +1,12 @@
+module(...)
+ip = "192.168.5.224" --gsoft server ip
+port = 25 --gosft server port
+server = "gesql" -- sql server
+database = "pubs" -- sql server database
+uid = "sa" --sql server user name
+pwd = "asd38fgh" -- sql server user possword
+tb = "[dbo].[gsoft]" -- sql server table name (username=ip,starttime,enddatetime,totaltimes,appname)
+log_file = true
+log_sql = true 
+trace = true 
+
---
+++ test.lua
@@ -0,0 +1,41 @@
+package.cpath = "./?51.dll;./?.dll"
+require "ado"
+require "config"
+
+--[[
+local db_string = "Provider=SQLOLEDB;Server=gesql;Database=pubs;uid=sa;pwd=asd38fgh"
+local db = ADO_Open(db_string)
+
+local function insertdata(username,startdatetime,enddatetime,totaltimes,appname)
+local str = "INSERT INTO [dbo].[gsoft] (username,startdatetime,enddatetime,totaltimes,appname) VALUES ('" .. username .. "','" .. startdatetime .. "','" .. enddatetime .. "','"  .. totaltimes .. "','" .. appname .. "')"
+print(str .. "\n")
+db:exec(str)
+end
+insertdata("192.168.5.244",os.date("%Y-%m-%d %X"),os.date("%Y-%m-%d %X"),100,"gosft")
+--insertdata("192.168.5.244",os.date("%x %X"),os.date("%x %X"),100,"gosft")
+db:close()
+--]]
+
+local function open_db(server,database,uid,pwd)
+	local db_string = string.format("Provider=SQLOLEDB;Server=%s;Database=%s;uid=%s;pwd=%s",server,database,uid,pwd)
+	print(db_string .. "\n")	
+	local db = ADO_Open(db_string)
+
+	return db
+end
+
+local function insertdata(db,tb,username,startdatetime,enddatetime,totaltimes,appname)
+	local str = "INSERT INTO " .. tb  .. "(username,startdatetime,enddatetime,totaltimes,appname) VALUES ('" .. username .. "','" .. startdatetime .. "','" .. enddatetime .. "','"  .. totaltimes .. "','" .. appname .. "')"
+	print(str .. "\n")
+	db:exec(str)
+end
+
+local function fmt_data(datetime)
+	return os.date("%Y-%m-%d %X",datetime)
+end
+
+
+local db = ADO_Create()
+--local	db = open_db(config.server,config.database,config.uid,config.pwd)
+--insertdata(db,config.tb,"192.168.5.244",fmt_data(os.time()),fmt_data(os.time()),100,"goft")
+--db:close()
